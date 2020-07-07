605e8b5cbbfc75ab15abdba0503117f3c25102b5
Ajengwahyu <ajeng_wahyu_27rpl@student.smktelkom-mlg.sch.id>
2020-07-07 19:16:52 +0700

    first

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+        <html lang="en">
+        
+        <head>
+        
+          <meta charset="utf-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+          <meta name="description" content="">
+          <meta name="author" content="">
+        
+          <title>optima</title>
+        
+          <!-- Bootstrap core CSS -->
+          <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        
+          <!-- Custom styles for this template -->
+          <link href="css/shop-homepage.css" rel="stylesheet">
+        
+        </head>
+        
+        <body>
+        
+          <!-- Navigation -->
+          <nav class="navbar navbar-expand-lg navbar-light fixed-top">
+            <div class="container">
+              <a class="navbar-brand" href="#">
+                <img src="img/header.jpg" alt="">
+              </a>
+              <ul class="navbar-nav ml-auto">
+                <li class="nav-item"><h6 class="nav-link">Spare Part & Peralatan Kapal</h6></li>
+              </ul>
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                  <li class="nav-item active">
+                    <a class="nav-link" href="#">Profil
+                      <span class="sr-only">(current)</span>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Hubungi Kami</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Blog</a>
+                  </li>
+                  <li class="nav-item">
+                    <h6 class="nav-link">Whatsapp</h6>
+                  </li>
+                  <!-- <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                  </li> -->
+                </ul>
+              </div>
+            </div>
+          </nav>
+        
+          <!-- Page Content -->
+          <div class="container">
+        
+            <div class="row">
+              <div class="col-lg-3">
+              </div>
+              <!-- /.col-lg-3 -->
+        
+              <div class="col-lg-9">
+        
+                <div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel">
+                  <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                  </ol>
+                  <div class="carousel-inner" role="listbox">
+                    <div class="carousel-item active">
+                      <img class="d-block img-fluid" src="img/index_03.gif" alt="First slide">
+                    </div>
+                    <div class="carousel-item">
+                      <img class="d-block img-fluid" src="img/index_05.gif" alt="Second slide">
+                    </div>
+                    <div class="carousel-item">
+                      <img class="d-block img-fluid" src="img/index_07.gif" alt="Third slide">
+                    </div>
+                  </div>
+                  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                  </a>
+                  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                  </a>
+                </div>
+        
+                <div class="row">
+        
+                  <div class="col-lg-4 col-md-6 mb-4">
+                    <div class="card">
+                      <a href="#"><img class="card-img-top" src="img/index_09.gif" alt=""></a>
+                      <div class="card-body">
+                        <h4 class="card-title">
+                          <a href="#">Panel & Gauge Kapal</a>
+                        </h4>
+                        <h5>12.000.000</h5>
+                      </div>
+                    </div>
+                  </div>
+        
+                  <div class="col-lg-4 col-md-6 mb-4">
+                    <div class="card">
+                      <a href="#"><img class="card-img-top" src="img/index_16.gif" alt=""></a>
+                      <div class="card-body">
+                        <h4 class="card-title">
+                          <a href="#">Spare Part Kapal</a>
+                        </h4>
+                        <h5>12.000.000</h5>
+                      </div>
+                    </div>
+                  </div>
+        
+                  <div class="col-lg-4 col-md-6 mb-4">
+                    <div class="card">
+                      <a href="#"><img class="card-img-top" src="img/index_18.gif" alt=""></a>
+                      <div class="card-body">
+                        <h4 class="card-title">
+                          <a href="#">Spare Part Kapal</a>
+                        </h4>
+                        <h5>12.000.000</h5>
+                      </div>
+                    </div>
+                  </div>
+        
+                  <div class="col-lg-4 col-md-6 mb-4">
+                    <div class="card">
+                      <a href="#"><img class="card-img-top" src="img/index_25.gif" alt=""></a>
+                      <div class="card-body">
+                        <h4 class="card-title">
+                          <a href="#">Spare Part Kapal</a>
+                        </h4>
+                        <h5>12.000.000</h5>
+                      </div>
+                    </div>
+                  </div>
+        
+                  <div class="col-lg-4 col-md-6 mb-4">
+                    <div class="card">
+                      <a href="#"><img class="card-img-top" src="img/index_27.gif" alt=""></a>
+                      <div class="card-body">
+                        <h4 class="card-title">
+                          <a href="#">Spare Part Kapal</a>
+                        </h4>
+                        <h5>12.000.000</h5>
+                      </div>
+                    </div>
+                  </div>
+        
+                  <div class="col-lg-4 col-md-6 mb-4">
+                    <div class="card">
+                      <a href="#"><img class="card-img-top" src="img/index_29.jpg" alt=""></a>
+                      <div class="card-body">
+                        <h4 class="card-title">
+                          <a href="#">Spare Part Kapal</a>
+                        </h4>
+                        <h5>12.000.000</h5>
+                      </div>
+                    </div>
+                  </div>
+        
+                </div>
+                <!-- /.row -->
+        
+              </div>
+              <!-- /.col-lg-9 -->
+        
+            </div>
+            <!-- /.row -->
+        
+          </div>
+          <!-- /.container -->
+        
+          <!-- Footer -->
+          <footer class="py-5 bg-light">
+            <div class="container">
+            <p class="m-0 text-center text-black ">Copyright @ 2020 OPTIMA Parts<br> Menjuan aneka parts & peralatan kapal lainnya. <br>All rights reserved.</p>
+            </div>
+            <!-- /.container -->
+          </footer>
+        
+          <!-- Bootstrap core JavaScript -->
+          <script src="vendor/jquery/jquery.min.js"></script>
+          <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+        
+        </body>
+        
+        </html>
+        
